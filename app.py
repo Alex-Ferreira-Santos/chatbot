cfa5851c6bc1cbc0df54c77287515bc316461dea
Alex Ferreira Santos <alexsantos@pecege.com>
2024-05-15 20:18:24 -0300

    feat: adicionando funcionamento do bot com llama3 e adicionando CORS

--- app.py
+++ app.py
@@ -1,20 +1,63 @@
+from time import sleep
 from flask import Flask,render_template, request, Response
-from openai import OpenAI
+from flask_cors import CORS, cross_origin
+from groq import Groq
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-modelo = "gpt-4"
+cliente = Groq(api_key=os.getenv("GROQ_API_KEY"))
+modelo = "llama3-8b-8192"
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = 'Content-Type'
 app.secret_key = 'alura'
 
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+            prompt_do_sistema = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do e-commerce informado!
+            """
+            response = cliente.chat.completions.create(
+                messages=[
+                        {
+                            "role": "system",
+                            "content": prompt_do_sistema
+                        },
+                        {
+                            "role": "user",
+                            "content": prompt
+                        }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model = modelo)
+            return response
+        except Exception as erro:
+                repeticao += 1
+                if repeticao >= maximo_tentativas:
+                        return "Erro no GPT: %s" % erro
+                print('Erro de comunicação com OpenAI:', erro)
+                sleep(1)
+
+
 @app.route("/chat", methods=["POST"])
+@cross_origin()
 def chat():
     prompt = request.json["msg"]
     resposta = bot(prompt)
+    texto_resposta = resposta.choices[0].message.content
+    return texto_resposta
 
 @app.route("/")
 def home():
